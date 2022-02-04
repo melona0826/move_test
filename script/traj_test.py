9b07ce71e3ce84bccc2633da13758094c93f8b0f
Kwon Jin <kwonjin@kaist.ac.kr>
2022-02-04 16:22:41 +0900

    Inital Commit

---
+++ script/traj_test.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+#
+# Copyright 2015, 2016 Thomas Timm Andersen (original version)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+import roslib; roslib.load_manifest('ur_driver')
+import rospy
+import actionlib
+from control_msgs.msg import *
+from trajectory_msgs.msg import *
+from sensor_msgs.msg import JointState
+from math import pi
+
+JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
+               'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
+
+def deg_2_rad(val) :
+    return val * pi /180
+
+Q1 = [-0.04483205476869756, -0.733215634022848, -2.0507729689227503, -1.6661017576800745, 1.5725396871566772, -0.004635636006490529]
+
+
+client = None
+
+def move1(target_goal):
+    global joints_pos
+    g = FollowJointTrajectoryGoal()
+    g.trajectory = JointTrajectory()
+    g.trajectory.joint_names = JOINT_NAMES
+    try:
+        joint_states = rospy.wait_for_message("joint_states", JointState)
+        joints_pos = joint_states.position
+        g.trajectory.points = [
+            JointTrajectoryPoint(positions=joints_pos, velocities=[0]*6, time_from_start=rospy.Duration(0.0)),
+            JointTrajectoryPoint(positions=target_goal, velocities=[0]*6, time_from_start=rospy.Duration(2.0))]
+        client.send_goal(g)
+        client.wait_for_result()
+    except KeyboardInterrupt:
+        client.cancel_goal()
+        raise
+    except:
+        raise
+
+def main():
+    global client
+    try:
+        rospy.init_node("test_move", anonymous=True, disable_signals=True)
+        client = actionlib.SimpleActionClient('follow_joint_trajectory', FollowJointTrajectoryAction)
+        print "Waiting for server..."
+        client.wait_for_server()
+        print "Connected to server"
+        parameters = rospy.get_param(None)
+        index = str(parameters).find('prefix')
+        if (index > 0):
+            prefix = str(parameters)[index+len("prefix': '"):(index+len("prefix': '")+str(parameters)[index+len("prefix': '"):-1].find("'"))]
+            for i, name in enumerate(JOINT_NAMES):
+                JOINT_NAMES[i] = prefix + name
+
+        print "Please make sure that your robot can move freely between these poses before proceeding!"
+        inp = raw_input("Continue? y/n: ")[0]
+        if (inp == 'y'):
+            move1(Q1)
+
+        else:
+            print "Halting program"
+    except KeyboardInterrupt:
+        rospy.signal_shutdown("KeyboardInterrupt")
+        raise
+
+if __name__ == '__main__':
+    main()
